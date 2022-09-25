9d66c41c31f2c9fcd89e7911b5d6c9cb85d327a1
capdatasystem <flavius_marc@yahoo.fr>
2022-09-25 00:16:36 +0000

    start

---
+++ blockchain.py
@@ -0,0 +1,19 @@
+
+blockchain = []
+
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount, last_transaction):
+    blockchain.append([last_transaction, transaction_amount])
+    print(blockchain)
+
+
+add_value(5.2, [1])
+add_value(6.5, get_last_blockchain_value())
+add_value(4.5, get_last_blockchain_value())
+
+
+print(blockchain)
