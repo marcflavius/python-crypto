098e03b167018faee0d063b5faedd9743fe640ac
capdatasystem <flavius_marc@yahoo.fr>
2022-09-25 01:53:53 +0000

    [master] 📦 NEW: Type casting

--- blockchain.py
+++ blockchain.py
@@ -5,15 +5,20 @@ blockchain = []
 def get_last_blockchain_value():
     return blockchain[-1]
 
-
-def add_value(transaction_amount, last_transaction):
+def get_user_input(msg):
+    return float(input(msg+': \n'))
+    
+def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
 
-add_value(5.2, [1])
-add_value(6.5, get_last_blockchain_value())
-add_value(4.5, get_last_blockchain_value())
+tx_amount = get_user_input('Your transaction please: ')
+add_value(tx_amount)
+tx_amount = get_user_input('Your transaction please: ')
+add_value(tx_amount)
+tx_amount = get_user_input('Your transaction please: ')
+add_value(tx_amount)
 
 
 print(blockchain)
