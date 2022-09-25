e326d04e05ed4652ba1dee29e29c4643bf5cd571
capdatasystem <flavius_marc@yahoo.fr>
2022-09-25 12:10:46 +0000

    [master] 🐛 FIX: fix init blockchain logic

--- blockchain.py
+++ blockchain.py
@@ -4,30 +4,31 @@ blockchain = []
 
 def get_last_blockchain_value():
     """ Get the last element in the blockchain"""
-    return blockchain[-1]
+    return blockchain[-1] if len(blockchain) > 0 else [1]
 
 
-def get_user_input_func(msg):
+def get_user_input(msg):
     """ Get a user input """
     return float(input(msg+': \n'))
 
 
-def add_value_func(transaction_amount, last_transaction=[1]):
+def add_value(transaction_amount, last_transaction):
     """ Add a Item to the blockchain """
     blockchain.append([last_transaction, transaction_amount])
-    print(blockchain)
 
 
-def grouped_transaction(get_user_input, add_value):
+def grouped_transaction():
     """ Groupe transaction handler"""
     total_transaction = int(get_user_input("Please enter the number of transaction you wish to proceed"))
     for count in range(total_transaction):
         tx_amount = get_user_input('{}/{} Your transaction please: '.format(count+1, total_transaction))
-        add_value(tx_amount)
+        add_value(tx_amount, get_last_blockchain_value())
 
 
-grouped_transaction(get_user_input_func, add_value_func)
+grouped_transaction()
 
 
-print(blockchain)
+for block in blockchain:
+    print('Outpouting blck')
+    print(block)
 print('God Bye!')
