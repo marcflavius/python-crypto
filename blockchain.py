9a5396cf466edeb815f907aa271e67eaf8b66602
Marc Flavius <marc.flavius@atos.net>
2022-11-19 23:32:39 +0100

    New - add salt to mining

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,9 @@
 """ Blockchain lib """
 import hashlib as hash
-import json
 import re
 from functools import reduce
-genesis_block = {
-    "previous_hash": "",
-    "index": 0,
-    "transactions": []
-}
+
+genesis_block = {"previous_hash": "", "index": 0, "transactions": []}
 
 owner = "Marc"
 participants = set([owner])
@@ -17,7 +13,7 @@ MINING_TRANSACTION = 5
 
 
 def grouped_transaction():
-    """ Groupe transaction handler """
+    """Groupe transaction handler"""
     global participants
     global open_transaction
     global blockchain
@@ -44,7 +40,7 @@ def grouped_transaction():
         elif user_wants_to_output_the_blockchain(user_choice):
             output_blockchain(blockchain)
         elif user_wants_to_mind_new_block(user_choice):
-            mine_block()
+            mine_block(blockchain, open_transaction)
         elif user_wants_to_output_open_transaction(user_choice):
             output_open_transactions(open_transaction)
         elif user_wants_to_output_participants(user_choice):
@@ -53,47 +49,74 @@ def grouped_transaction():
             output_user_balance(owner)
         # choice not handled
         elif user_choice not in choice_list:
-            print('Input invalid, please choose a listed choice')
+            print("Input invalid, please choose a listed choice")
         else:
             # choice 3
             break
 
 
-def verify_chain():
-    """ Verify the blockchain integrity """
-    block_index = 0
+def verify_chain(blockchain):
+    """Verify the blockchain integrity"""
+    block_index = -1
     is_valid = True
+    previous_hash = ""
     for current_block in blockchain:
-        prev_block = blockchain[block_index - 1]["previous_hash"]
-        if current_block != prev_block:
+        block_index = block_index + 1
+        prev_index = block_index - 1
+        if _blockchain_has_zero_or_one_block(blockchain):
+            break
+        if _is_first_block(block_index):
+            # skip verification
+            continue
+        # compute previous hash
+        prev_block = blockchain[prev_index]
+        transactions = prev_block["transactions"]
+        salt = find_block_salt(transactions, previous_hash)
+        previous_hash = hash_block(salt, previous_hash, prev_block)
+        
+        if previous_hash != current_block["previous_hash"]:
             is_valid = False
             break
-    return is_valid if current_block["previous_hash"] == {} else True
+    return is_valid
+
+
+def _is_first_block(block_index):
+    return block_index == 0
+
+
+def _blockchain_has_multiple_block(blockchain):
+    return len(blockchain) > 1
+
+
+def _blockchain_has_zero_or_one_block(blockchain):
+    return len(blockchain) <= 1
 
 
 def reset_blockchain():
-    """ Wipe the blockchain """
+    """Wipe the blockchain"""
     global blockchain
     blockchain = []
 
 
-def get_last_blockchain_value():
-    """ Get the last element in the blockchain """
+def get_last_blockchain_value(givin_blockchain = None):
+    """Get the last element in the blockchain"""
     global genesis_block
     global blockchain
-    return blockchain[-1] if len(blockchain) > 0 else genesis_block
+    used_blockchain = blockchain if givin_blockchain == None else givin_blockchain
+    return used_blockchain[-1] if len(used_blockchain) > 0 else genesis_block
 
 
-def get_user_input(msg, append='\n'):
-    """ Get a user input """
+def get_user_input(msg, append="\n"):
+    """Get a user input"""
     output = input(msg + append)
     return output
+
 
 # TODO: Marc Flavius - fix sender
 
 
 def add_transaction(recipient, sender="Marc", amount=1.0):
-    """ Add a Item to the blockchain
+    """Add a Item to the blockchain
     sender: the sender of the coins.
     recipient: the recipient of the coins.
     amount: the given transaction amount
@@ -109,41 +132,42 @@ def add_transaction(recipient, sender="Marc", amount=1.0):
     participants.add(recipient)
 
 
-def mine_block():
-    """ Add all open transaction to a block and wipe
-    the open transaction array """
-    global blockchain
-    global open_transaction
+def mine_block(blockchain, open_transaction):
+    """Add all open transaction to a block and wipe
+    the open transaction array"""
+    previous_hash = get_last_blockchain_value(blockchain)["previous_hash"]
     reward_transaction = {
         "sender": "MINING",
         "recipient": owner,
         "amount": MINING_TRANSACTION,
     }
     open_transaction.append(reward_transaction)
-    hashed_block = hash_block()
+    # hashing
+    salt = find_block_salt(open_transaction, previous_hash)
+    current_hash = hash_block(open_transaction, previous_hash, salt)
     new_block = {
-        "previous_hash": hashed_block,
+        "previous_hash": current_hash,
         "index": len(blockchain),
         "transactions": open_transaction,
+        "salt": salt,
     }
     blockchain.append(new_block)
-    verify_chain()
+    verify_chain(blockchain)
     open_transaction = []
 
 
-def hash_block():
-    last_block = get_last_blockchain_value()
-    hashed_block = ""
-    for key in last_block:
-        value = last_block[key]
-        hashed_block = hashed_block + str(value)
-    return hash_object(hashed_block)
+def hash_block(open_transaction, previous_hash, salt):
+    return hash.sha256(
+        stringify_decoded_hash(open_transaction, previous_hash, salt)
+    ).hexdigest()
 
 
 def get_user_transaction():
-    """ Get user input transaction"""
-    recipient = ("" + get_user_input('Enter the recipient of the transaction: ')).capitalize()
-    tx_amount = get_numeric_input('Your transaction please: ')
+    """Get user input transaction"""
+    recipient = (
+        "" + get_user_input("Enter the recipient of the transaction: ")
+    ).capitalize()
+    tx_amount = get_numeric_input("Your transaction please: ")
     return tx_amount, recipient
 
 
@@ -151,17 +175,20 @@ def get_numeric_input(input):
     tx_amount = input
     match = len(re.findall("[0-9]+$", tx_amount))
     if match != 1:
-        print('Please enter a numeric amount\n')
+        print("Please enter a numeric amount\n")
         tx_amount = get_numeric_input(get_user_input(input))
     return int(tx_amount)
 
 
 def get_user_choice(user_operation_help):
-    """ Get user choice """
+    """Get user choice"""
     user_choice = get_user_input(user_operation_help)
     choice_list = [str(item + 1) for item in range(2)]
-    choice_list.append('q')
-    return user_choice, choice_list,
+    choice_list.append("q")
+    return (
+        user_choice,
+        choice_list,
+    )
 
 
 def user_wants_to_quit(user_choice):
@@ -169,35 +196,35 @@ def user_wants_to_quit(user_choice):
 
 
 def user_wants_to_add_a_new_transaction(user_choice):
-    """ Check for a new transaction operation """
-    return str(user_choice) == '1'
+    """Check for a new transaction operation"""
+    return str(user_choice) == "1"
 
 
 def user_wants_to_output_the_blockchain(user_choice):
-    """ Check for a output blockchain operation """
-    return str(user_choice) == '2'
+    """Check for a output blockchain operation"""
+    return str(user_choice) == "2"
 
 
 def user_wants_to_mind_new_block(user_choice):
-    return str(user_choice) == '3'
+    return str(user_choice) == "3"
 
 
 def user_wants_to_output_participants(user_choice):
-    return str(user_choice) == '4'
+    return str(user_choice) == "4"
 
 
 def user_wants_to_output_open_transaction(user_choice):
-    return str(user_choice) == '5'
+    return str(user_choice) == "5"
 
 
 def user_wants_to_output_balance(user_choice):
-    return str(user_choice) == '6'
+    return str(user_choice) == "6"
 
 
 def output_blockchain(given_blockchain):
-    """ Output the block chain """
+    """Output the block chain"""
     for block in given_blockchain:
-        print('Outputting block...')
+        print("Outputting block...")
         print(block)
 
 
@@ -214,13 +241,13 @@ def output_open_transactions(open_transactions):
 def get_user_balance(user_id):
     global blockchain
     last_block = get_last_blockchain_value()
-    transactions = last_block['transactions']
+    transactions = last_block["transactions"]
 
     def balance(total, transaction):
-        amount = transaction['amount']
-        if transaction['recipient'] == user_id:
+        amount = transaction["amount"]
+        if transaction["recipient"] == user_id:
             return total + amount
-        if transaction['sender'] == user_id:
+        if transaction["sender"] == user_id:
             return total - amount
         else:
             return total
@@ -235,11 +262,25 @@ def output_user_balance(user_id):
     return balance
 
 
-def hash_object(obj):
-    print('')
-    return hash.sha256(json.dumps(obj).encode()).hexdigest()
+def valid_salt(open_transaction, previous_hash, salt):
+    guess = stringify_decoded_hash(open_transaction, previous_hash, salt)
+    guess_hash = hash.sha256(guess).hexdigest()
+    return guess_hash[0:1] == "0"
+
+
+def stringify_decoded_hash(open_transaction, previous_hash, salt):
+    return (str(open_transaction) + str(previous_hash) + str(salt)).encode()
+
+
+def find_block_salt(transactions, previous_hash):
+    salt = -1
+    valid = False
+    while valid is not True:
+        salt = salt + 1
+        valid = valid_salt(transactions, previous_hash, salt)
+    return salt
 
 
 if __name__ == "__main__":
     grouped_transaction()
-    print('God Bye!')
+    print("God Bye!")
--- test_blockchain.py
+++ test_blockchain.py
@@ -1,6 +1,17 @@
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 import blockchain
+
+genesis_block = {
+    "previous_hash": "GENESIS_BLOCK",
+    "index": 0,
+    "transactions": [],
+}
+derived_from_genesis_block = {
+    "previous_hash": "076f74c1e78bceb14a65775013948f24eaacc3ac1bc7e3e911fe4aa3ae198c7",
+    "index": 0,
+    "transactions": [],
+}
 
 
 class Blockchain(unittest.TestCase):
@@ -9,33 +20,130 @@ class Blockchain(unittest.TestCase):
         blockchain.open_transaction = []
         blockchain.owner = "Marc"
         blockchain.participants = set({"Marc"})
-        blockchain.genesis_block = {
-            "previous_hash": "",
-            "index": 0,
-            "transactions": [],
-        }
+        blockchain.genesis_block = genesis_block
         return super().tearDown()
 
     def tearDown(self) -> None:
-        return super().tearDown() 
+        return super().tearDown()
+
+    # (start)
+    def test_mind_block(self):
+        given_blockchain = [genesis_block, derived_from_genesis_block]
+        open_transaction = []
+        blockchain.mine_block(given_blockchain, open_transaction)
+        self.assertEqual(len(given_blockchain), 3)
+        self.assertEqual(
+            given_blockchain[2]["previous_hash"],
+            "030e0c7cf43ce5ee6ff024185ee40566be4432c0966f74c47285352c7cf4263c",
+        )
+        self.assertEqual(given_blockchain[2]["index"], 2)
+        self.assertEqual(given_blockchain[2]["salt"], 15)
+        self.assertEqual(
+            given_blockchain[2]["transactions"],
+            [{"sender": "MINING", "recipient": "Marc", "amount": 5}],
+        )
+
+    @patch("blockchain.find_block_salt", return_value=22)
+    def test_verify_the_chain_with_one_block(self, find_block_salt):
+        blockchain.blockchain = [genesis_block]
+        valid = blockchain.verify_chain(blockchain.blockchain)
+        self.assertEqual(valid, True)
+        find_block_salt.called
+
+    @patch("blockchain.find_block_salt", return_value=22)
+    def test_verify_the_chain_with_multi_block_fail(self, find_open_transaction_salt):
+        blockchain.blockchain = [genesis_block, genesis_block]
+        valid = blockchain.verify_chain(blockchain.blockchain)
+        self.assertEqual(valid, False)
+        find_open_transaction_salt.called
+
+    @patch("blockchain.find_block_salt", return_value=22)
+    @patch(
+        "blockchain.hash_block",
+        return_value="076f74c1e78bceb14a65775013948f24eaacc3ac1bc7e3e911fe4aa3ae198c7",
+    )
+    def test_verify_the_chain_with_multi_block(self, find_block_salt, hash_block):
+        stub_blockchain = [genesis_block, derived_from_genesis_block]
+        valid = blockchain.verify_chain(stub_blockchain)
+        self.assertEqual(valid, True)
+        find_block_salt.called
+        hash_block.called
 
     # @unittest.skip
-    def test_get_user_balance(self):
-        blockchain.add_transaction(
-            recipient="Bob", sender=blockchain.owner, amount=100,)
-        blockchain.add_transaction(
-            recipient="Bob", sender=blockchain.owner, amount=100,)
-        blockchain.mine_block()
-        # marcBalance = blockchain.get_user_balance('Marc')
-        bobBalance = blockchain.get_user_balance('Bob')
-        # self.assertEqual(marcBalance,5)
+    @patch(
+        "blockchain.get_last_blockchain_value",
+        return_value={
+            "previous_hash": "076f74c1e78bceb14a65775013948f24eaacc3ac1bc7e3e911fe4aa3ae198c7"
+        },
+    )
+    def test_valid_salt(self, get_last_blockchain_value):
+        open_transaction = [{"sender": "Marc", "recipient": "Bob", "amount": 100}]
+        previous_hash = blockchain.get_last_blockchain_value()["previous_hash"]
+        salt = blockchain.find_block_salt(open_transaction, previous_hash)
+        valid = blockchain.valid_salt(open_transaction, previous_hash, 19)
+        get_last_blockchain_value.called
+        self.assertEqual(valid, True)
+
+    def test_find_block_salt(self):
+        open_transaction = [{"sender": "Marc", "recipient": "Bob", "amount": 100}]
+        previous_hash = (
+            "076f74c1e78bceb14a65775013948f24eaacc3ac1bc7e3e911fe4aa3ae198c7"
+        )
+        salt = blockchain.find_block_salt(open_transaction, previous_hash)
+        self.assertEqual(19, salt)
+
+    # @unittest.skip
+    @patch("blockchain.get_last_blockchain_value")
+    def test_invalid_proof(self, get_last_blockchain_value):
+        blockchain.open_transaction = self.get_open_transaction_stub()
+        valid = blockchain.valid_salt(blockchain.open_transaction, "previous_hash", 20)
+        get_last_blockchain_value.called
+        self.assertEqual(valid, False)
+
+    def get_open_transaction_stub(self):
+        return [{"sender": "Marc", "recipient": "Bob", "amount": 100}]
+
+    def get_blockchain_stub(self):
+        return [
+            genesis_block,
+            {
+                "previous_hash": "00",
+                "index": 1,
+                "transactions": [
+                    {"sender": "Marc", "recipient": "Bob", "amount": 100},
+                    {"sender": "Marc", "recipient": "Bob", "amount": 100},
+                ],
+            },
+        ]
+
+    # @unittest.skip
+    @patch(
+        "blockchain.get_last_blockchain_value",
+        return_value={
+            "transactions": [
+                {"sender": "Marc", "recipient": "Bob", "amount": 100},
+                {"sender": "Marc", "recipient": "Bob", "amount": 100},
+            ],
+        },
+    )
+    def test_get_user_balance(self, get_last_blockchain_value):
+        bobBalance = blockchain.get_user_balance("Bob")
+        marcBalance = blockchain.get_user_balance("Marc")
         self.assertEqual(bobBalance, 200)
+        self.assertEqual(marcBalance, -200)
+        get_last_blockchain_value.called
 
     # @unittest.skip
     def test_it_can_add_a_transaction_to_the_open_transaction_queue(self):
-        blockchain.add_transaction(recipient="Bob", sender="Marc", amount=10,)
-        self.assertEqual(blockchain.open_transaction, [
-                         {'amount': 10, 'recipient': 'Bob', 'sender': 'Marc'}])
+        blockchain.add_transaction(
+            recipient="Bob",
+            sender="Marc",
+            amount=10,
+        )
+        self.assertEqual(
+            blockchain.open_transaction,
+            [{"amount": 10, "recipient": "Bob", "sender": "Marc"}],
+        )
 
     # def test_it_can_output_a_given_blockchain(self):
     #     blockchain.add_transaction(25, blockchain.get_last_blockchain_value())
@@ -49,54 +157,47 @@ class Blockchain(unittest.TestCase):
         self.assertEqual(blockchain.participants, set({"Paul", "Marc"}))
 
     # @unittest.skip
-    def test_blockchain_is_valid(self):
+    def int_test_blockchain_is_valid(self):
+        stub_blockchain = []
+        open_transaction = []
         blockchain.add_transaction(recipient="Bob", sender="Marc", amount=10)
-        blockchain.mine_block()
+        blockchain.mine_block(stub_blockchain, open_transaction)
         blockchain.add_transaction(recipient="Tom", sender="Marc", amount=10)
-        blockchain.mine_block()
-        is_valid = blockchain.verify_chain()
+        blockchain.mine_block(stub_blockchain, open_transaction)
+        is_valid = blockchain.verify_chain(blockchain)
         self.assertTrue(is_valid)
 
-    # @unittest.skip
-    @patch('blockchain.grouped_transaction', return_value=1)
-    def test_blockchain_add_transaction(self, mockedGroupTransaction):
-        assert mockedGroupTransaction.notCalled
-        blockchain.add_transaction(recipient="Bob", sender="Marc", amount=10)
-        blockchain.mine_block()
-        blockchain.add_transaction(recipient="Test", sender="Marc", amount=10)
-        blockchain.mine_block()
-
-        is_valid = blockchain.verify_chain()
-        self.assertTrue(is_valid)
-
-    @patch('blockchain.get_user_input', side_effect=["1", "Bob", "10", "q"])
+    @patch("blockchain.get_user_input", side_effect=["1", "Bob", "10", "q"])
     def test_add_transaction_api(self, get_user_input):
         blockchain.grouped_transaction()
         assert get_user_input.called
 
-    def test_hash_a_blockchain(self):
-        blockchain.add_transaction(recipient="Tom", sender="Marc", amount=10)
-        blockchain.mine_block()
-        hashed_blockchain = blockchain.hash_block()
-        last_hash = "3acc5ced004146f444dbbefb339fa673994eb6553811f9511fce548c12f30f2e"
-        self.assertEqual(hashed_blockchain, last_hash)
+    @patch("blockchain.get_last_blockchain_value")
+    def test_hash_a_blockchain(self, get_last_blockchain_value):
+        previous_hash = (
+            "076f74c1e78bceb14a65775013948f24eaacc3ac1bc7e3e911fe4aa3ae198c7"
+        )
+        salt = 0
+        hash = ""
+        while hash[0:1] != "0":
+            salt = salt + 1
+            hash = blockchain.hash_block([], previous_hash, salt)
+        get_last_blockchain_value.called
+        self.assertEqual(hash[0:1], "0")
 
     # @unittest.skip
     def test_it_can_list_participants(self):
         participants = set({"Marc"})
-        self.assertEqual(
-            blockchain.output_participant(participants),
-            participants)
+        self.assertEqual(blockchain.output_participant(participants), participants)
 
     # @unittest.skip
     def test_it_can_output_user_balance(self):
-        blockchain.add_transaction(recipient="Tom", sender="Marc", amount=10)
-        blockchain.mine_block()
-        self.assertEqual(blockchain.get_user_balance('Marc'), -5)
+        blockchain.blockchain = self.get_blockchain_stub()
+        self.assertEqual(blockchain.get_user_balance("Marc"), -200)
 
     # @unittest.skip
     # def test_it_can_mine_open_transactions(self):
-        # self.assertEqual(blockchain.output_user_balance())
+    # self.assertEqual(blockchain.output_user_balance())
 
     # @unittest.skip
     def test_user_wants_to_add_a_new_transaction(self):
@@ -123,5 +224,5 @@ class Blockchain(unittest.TestCase):
         self.assertFalse(blockchain.user_wants_to_add_a_new_transaction(None))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
