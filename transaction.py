a56489c52edb6bbdc91812e91bb232eb69f6a6d8
Mc-fly82 <flavius_marc@yahoo.fr>
2022-12-01 19:10:08 +0100

    New - Add transaction class

---
+++ transaction.py
@@ -0,0 +1,15 @@
+from time import time
+
+
+class Transaction:
+    def __init__(self, transaction):
+        self.sender = transaction["sender"]
+        self.recipient = transaction["recipient"]
+        self.amount = transaction["amount"]
+        if hasattr(transaction, 'created_at'):
+            self.created_at = transaction["created_at"]
+            return 
+        self.created_at = time()
+    def __repr__(self):
+        return self.__dict__
+ 
